99446966d1014598594f74179751fe3bb0be51f9
ThiagoSCarv <thiagocarv4@gmail.com>
2025-07-03 19:51:33 -0300

    persistindo dados na aplicação

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import http from "node:http"
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === "POST" && url === "/users"){
+    users.push({
+      id: 1,
+      name: "Thiago",
+      email: "thiagocarv4@gmail.com"
+    })
+    return res.end("Criação de usúarios")
+  }
+
+  if (method === "GET" && url === "/users") {
+    res.setHeader("Content-Type", "application/json")
+    return res.end(JSON.stringify(users))
+  }
+
+  return res.end("Hello World")
+})
+
+server.listen(3333)
