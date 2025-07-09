4e38df2ea7c970a72acf52e5a920702e2135017c
ThiagoSCarv <thiagocarv4@gmail.com>
2025-07-08 21:39:25 -0300

    Atualizando registros

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -4,7 +4,7 @@ import { buildRoutePath } from "../utils/build-route-path.js"
 
 const database = new Database()
 
-export const routes = [ 
+export const routes = [
   {
     method: "GET",
     path: buildRoutePath("/users"),
@@ -14,23 +14,23 @@ export const routes = [
 
       return res.end(JSON.stringify(users))
     }
-  }, 
+  },
   {
     method: "POST",
-    path: buildRoutePath("/users"), 
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
-        const {name, email} = req.body
+      const { name, email } = req.body
 
-    
-        const user = {
-          id: randomUUID(),
-          name,
-          email, 
-        }
 
-        database.insert("users", user)
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      }
 
-        return res.writeHead(201).end()
+      database.insert("users", user)
+
+      return res.writeHead(201).end()
     }
   },
   {
@@ -40,6 +40,22 @@ export const routes = [
       const { id } = req.params
 
       database.delete('users', id)
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email
+      })
 
       return res.writeHead(204).end()
     },
